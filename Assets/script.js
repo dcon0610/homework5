57d2c9fe4a39b104a2db0ed39cba8871d145852a
David Connell <davecon88@gmail.com>
2020-09-21 11:57:25 +0930

    last commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,6 @@
 //initialise jquery
 $(document).ready(function(){
+    colorTable()
     // for (i=0; i<9; i++)
     // var setID=i
   
@@ -12,12 +13,11 @@ $(document).ready(function(){
     $( "#currentDay" ).text(currentTime)
 
 
-    $( "#9").click(function() {
+    $( "#b9").click(function() {
     
        var inputID="#1"
        var event = ($(inputID). val())
        if (event !="") {
-       console.log(event)
        var k=localStorage.length
        localStorage.setItem(k+1,event + inputID)
        }
@@ -25,8 +25,117 @@ $(document).ready(function(){
 
 
     })
+    $( "#b10").click(function() {
+    
+        var inputID="#2"
+        var event = ($(inputID). val())
+        if (event !="") {
+        var k=localStorage.length
+        localStorage.setItem(k+1,event + inputID)
+        }
+        else return
+ 
+ 
+     })
+     $( "#b11").click(function() {
+    
+        var inputID="#3"
+        var event = ($(inputID). val())
+        if (event !="") {
+        var k=localStorage.length
+        localStorage.setItem(k+1,event + inputID)
+        }
+        else return
+ 
+ 
+     })
+     $( "#b12").click(function() {
+    
+        var inputID="#4"
+        var event = ($(inputID). val())
+        if (event !="") {
+        var k=localStorage.length
+        localStorage.setItem(k+1,event + inputID)
+        }
+        else return
+ 
+ 
+     })
+     $( "#b13").click(function() {
+    
+        var inputID="#5"
+        var event = ($(inputID). val())
+        if (event !="") {
+        var k=localStorage.length
+        localStorage.setItem(k+1,event + inputID)
+        }
+        else return
+ 
+ 
+     })
+     $( "#b14").click(function() {
+    
+        var inputID="#6"
+        var event = ($(inputID). val())
+        if (event !="") {
+        var k=localStorage.length
+        localStorage.setItem(k+1,event + inputID)
+        }
+        else return
+ 
+ 
+     })
+     $( "#b15").click(function() {
+    
+        var inputID="#7"
+        var event = ($(inputID). val())
+        if (event !="") {
+        var k=localStorage.length
+        localStorage.setItem(k+1,event + inputID)
+        }
+        else return
+ 
+ 
+     })
+     $( "#b16").click(function() {
+    
+        var inputID="#8"
+        var event = ($(inputID). val())
+        if (event !="") {
+        var k=localStorage.length
+        localStorage.setItem(k+1,event + inputID)
+        }
+        else return
+ 
+ 
+     })
+
+    function colorTable(){
+        var input =[]
+        for (i=0; i<8; i++){
+            input[i]=i
+
+        }
+      
+        var currentTime = moment().format('HH');
+        console.log(currentTime)
+        for (i=9; i<17; i++){
+            console.log(i)
+            if(i<currentTime){
+            $("#"+i).css({"background-color":"grey"})
+            }
+             else if(i-currentTime===0){
+                $("#"+i).css({"background-color":"red"})
+                }
+            else{
+                $("#"+i).css({"background-color":"green"})
+                }
+           
 
 
+        }
+
+    }
 
 
 
@@ -40,8 +149,8 @@ $(document).ready(function(){
 
     function currentTime() {
 
-        var x = moment().format('MMMM Do YYYY');
-        return x
+        var currentDay = moment().format('MMMM Do YYYY');
+        return currentDay
     }
     
    
